5bfc3a19e3b952cb50b18bb382e33d3dcc4f1575
Eliezer Wohl <eliezerwohl@gmail.com>
2017-11-04 10:28:10 -0400

    fixed menu scrolling issue.  works in ie.  Bottom container is no longer page height, looks better on tablet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var menuVar;
-var compensate = 0;
+var current;
+
 function scrollFunction(){
   if($(this).scrollTop() > 0){   
       $('.navbar-default').removeClass("topNav").addClass("scrollNav");   
@@ -12,13 +13,19 @@ function scrollFunction(){
   }
 }
 function menusScroll (data){
-if (((document.documentElement.scrollTop||document.body.scrollTop) >= data.offsetTop - 80)
-	&& ((document.documentElement.scrollTop||document.body.scrollTop) <=  data.offsetTop + data.clientHeight - 80)){
-	if (menuVar != data.id) {
+	var id = data.id
+	//if scrolled at bottom
+	//document.body.scrollTop is an IE fix
+	if ((window.innerHeight + (document.documentElement.scrollTop || document.body.scrollTop)) >= document.body.offsetHeight - 70) {
+ 		id = "contact"
+  }
+
+if (((document.documentElement.scrollTop||document.body.scrollTop) >= data.offsetTop - 70)
+	&& ((document.documentElement.scrollTop||document.body.scrollTop) <=  data.offsetTop + data.clientHeight - 70)){
+	if (current != id) {
 	  $(".nav").find(".active").removeClass("active");
-			menuVar = data.id;
-			menuVar += "Button";
-    	$("#" + menuVar).parent().addClass("active");
+			current = id;
+    	$("#" + id + "Button").parent().addClass("active");
 		}
 	}
 }
@@ -36,10 +43,14 @@ $( document ).ready(function() {
 	var home = document.getElementById("home");
 	var contact = document.getElementById("contact");
 	var array = [aboutUs, portfolio, home, contact];
-  $(document).scroll(function(){
-  	for (var i = 0; i < array.length; i++) {
+	function menusScrollLoop() {
+		for (var i = 0; i < array.length; i++) {
   		menusScroll(array[i])
 	  }
+	}
+	menusScrollLoop()
+  $(document).scroll(function(){
+  	menusScrollLoop()
     scrollFunction()
 	});
   $('a[href*="#"]:not([href="#"])').click(function() {
