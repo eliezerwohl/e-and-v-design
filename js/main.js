c542b1ee27631344944e4163eb4ba5353bfbf8a3
Eliezer Wohl <eliezerwohl@gmail.com>
2017-12-22 17:16:02 -0500

    reenabled the javascript and modified css for navBar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 // var menuVar;
 // var current;
 
-// function scrollFunction(){
-//   if($(this).scrollTop() > 0){   
-//       $('.navbar-default').removeClass("topNav").addClass("scrollNav");   
-//       $(".navbar-brand").removeClass("brandHide").addClass("brandShow");
-//      } 
-//   else { 
-//     $('.navbar-default').removeClass("scrollNav").addClass("topNav");
-//     $(".navbar-brand").removeClass("brandShow").addClass("brandHide");
-//     $(".nav").find(".active").removeClass("active");
-//   }
-// }
+function scrollFunction(){
+	console.log("Yup")
+  if($(this).scrollTop() > 0){   
+      $('.navbar-default').removeClass("topNav").addClass("scrollNav");   
+      $(".navbar-brand").removeClass("brandHide").addClass("brandShow");
+     } 
+  else { 
+    $('.navbar-default').removeClass("scrollNav").addClass("topNav");
+    $(".navbar-brand").removeClass("brandShow").addClass("brandHide");
+    // $(".nav").find(".active").removeClass("active");
+  }
+}
 // function menusScroll (data){
 // 	var id = data.id
 // 	//if scrolled at bottom
@@ -29,17 +30,16 @@
 // 		}
 // 	}
 // }
-
 // $(".navbar-toggle, .nav  .navbar-collapse a").on("click", function(e) {
 // 	$("#bs-example-navbar-collapse-1").slideToggle("fast");
 // });
 // $(".navbar-brand").on("click", function() {
 //   $("#bs-example-navbar-collapse-1").slideUp("fast");
 // });
-// $( document ).ready(function() {
-// 	ajaxCall();
+$( document ).ready(function() {
+	ajaxCall();
 
-//   scrollFunction()
+  scrollFunction();
 // 	var aboutUs = document.getElementById("aboutUs");
 // 	var portfolio = document.getElementById("portfolio");
 // 	var home = document.getElementById("home");
@@ -51,10 +51,9 @@
 // 	  }
 // 	}
 // 	menusScrollLoop()
-//   $(document).scroll(function(){
-//   	menusScrollLoop()
-//     scrollFunction()
-// 	});
+  $(document).scroll(function(){
+    scrollFunction()
+	});
 //   $('a[href*="#"]:not([href="#"])').click(function() {
 //   	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 // 		  var target = $(this.hash);
@@ -67,41 +66,40 @@
 // 		  }
 // 		}
 // 	});
-// }); 
+}); 
 
-// function ajaxCall(){
+function ajaxCall(){
 
-// $("#submit").click(function(e){
-// 	e.preventDefault()
-// 	debugger
-// var name = $("#name").val();
-// var email = $("#email").val();
-// var message = $("#message").val();
-// // Returns successful data submission message when the entered information is stored in database.
-// var dataString = 'name='+ name + '&email='+ email + '&message='+ message;
-// $(".message").empty();
-// if(name==''||email==''||message=='')
-// {
+$("#submit").click(function(e){
+	e.preventDefault()
+var name = $("#name").val();
+var email = $("#email").val();
+var message = $("#message").val();
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name='+ name + '&email='+ email + '&message='+ message;
+$(".message").empty();
+if(name==''||email==''||message=='')
+{
 
-// $(".message").append("<span class='warning'>Please fill all fields</span>");
-// }
-// else
-// {
-// // AJAX Code To Submit Form.
-// $.ajax({
-// type: "POST",
-// url: "form.php",
-// data: dataString,
-// cache: false,
-// success: function(result){
-// 	$("#name").val("");
-// $("#email").val("");
-// $("#message").val("");
-// $(".message").append("<span class='success'>Thank you for contacting us, we will be in touch soon</span>");
-// }
-// });
-// }
-// return false;
-// });
+$(".message").append("<span class='warning'>Please fill all fields</span>");
+}
+else
+{
+// AJAX Code To Submit Form.
+$.ajax({
+type: "POST",
+url: "form.php",
+data: dataString,
+cache: false,
+success: function(result){
+	$("#name").val("");
+$("#email").val("");
+$("#message").val("");
+$(".message").append("<span class='success'>Thank you for contacting us, we will be in touch soon</span>");
+}
+});
+}
+return false;
+});
 
-// }
+}
