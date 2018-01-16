c4eb8e4c2b63eb98d6536566e18dade930590b81
Eliezer Wohl <eliezerwohl@gmail.com>
2018-01-16 12:36:40 -0500

    added biz schema

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer itemscope itemtype="http://schema.org/LocalBusiness" class="text-center">
+  <meta itemprop="email" content="eandvdesign@gmail.com">
+  <meta itemprop="image" content="https://eandvdesign.com/img/logo-5.png">
+  <meta itemprop="telephone" content="7325100423">
+  <span itemtype="http://schema.org/PostalAddress" itemscope="" itemprop="address">
+  <meta itemprop="streetAddress" content="112 Highland Ave">112 Highland Ave
+  <meta itemprop="addressLocality" content="Edison"> 
+  <meta itemprop="addressRegion" content="NJ">
+  <meta itemprop="postalCode" content="08817"></span>
+  <p>&copy; <?php echo date("Y"); ?> <span itemprop="name">E and V Design, LLC</span>.  All rights reserved.</p>
+  <p class="contact">
+  <a href="/contact">Contact us <img src="img/envelope.png"></a>
+  </p>
+</footer>
