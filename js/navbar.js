6b0f7a4facd959416025c09a31097975e03895e1
Eliezer Wohl <eliezerwohl@gmail.com>
2018-02-28 12:58:26 -0500

    created new navbar js without color and fading effect

--- js/navbar.js
+++ js/navbar.js
@@ -1,28 +1,40 @@
-$( document ).ready(function() {
-  scrollFunction();
+$(document).ready(function() {
   var navbar = $(".navbar-default");
-  $(document).scroll(function(){
-    scrollFunction()
-	});
-  function scrollFunction(){
-    var scrollin = $(this).scrollTop();
-    var mathin = Math.floor(scrollin / 4);
-    if (scrollin == 0){
+  var navbarLink = $(".navbar-nav li a");
+  // var num1 = 73;
+  // var num2 = 61;
+  // var num3 = 61;
+  function multi(currentPos) {
+    return (248 - (21 * currentPos)) + ", " + (248 - (17 * currentPos)) + ", " + (248 - (19 * currentPos));
+  }
+  function scrollFunction() {
+    var scrollPosition = $(this).scrollTop();
+    var mathNum = Math.floor(scrollPosition / 3);
+    if (scrollPosition == 0) {
       $('.navbar-default').removeClass("scrollNav").addClass("topNav");
       $(".navbar-brand").removeClass("brandShow").addClass("brandHide");
       $(navbar).css("background-color", "rgba(255,255,255,0.0")
-    }
-    else {
+      $(navbarLink).css("color", "rgb(248, 248, 248)")
+    } else {
       $('.navbar-default').removeClass("topNav").addClass("scrollNav");
       $(".navbar-brand").removeClass("brandHide").addClass("brandShow");
-      if (mathin < 10) {
-        mathin = 1;
+      if (mathNum < 10) {
+        mathNum = 1;
+      } else if (mathNum > 70) {
+        mathNum = 7;
       }
-      else if (mathin > 70){
-        mathin = 7;
-      }
-      var numin = "0." + parseInt(mathin.toString()[0]);
+      var numin = "0." + parseInt(mathNum.toString()[0]);
       $(navbar).css("background-color", "rgba(255,255,255," + numin + ")")
+      if (mathNum == 7) {
+        $(navbarLink).css("color", "rgb(152, 128, 136)")
+      } else {
+        $(navbarLink).css("color", "rgb(" + multi(mathNum.toString()[0]) + ")")
+      }
     }
   }
+  scrollFunction();
+  $(document).scroll(function() {
+    scrollFunction()
+  });
+
 });
